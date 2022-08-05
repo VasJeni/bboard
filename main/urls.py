c5d321057fe33ca76c9a2c4dbac4db2b022ad3c9
VasJeni <vasilyev508@gmail.com>
2022-08-05 12:32:58 +0100

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import index, other_page, BBLoginView, profile, BBLogoutView
+
+
+app_name = "main"
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:page>/', other_page, name='other'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout')
+]
+
